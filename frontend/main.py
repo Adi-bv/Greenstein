e3ff8dc7e6ec64a4d16bfc4660ae8c795cf0c2c1
Adi-bv <svaditi029@gmail.com>
2025-06-10 22:40:36 +0530

    initial commit

---
+++ frontend/main.py
@@ -0,0 +1,5 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+TOKEN: Final = '7905624830:AAEGDOfk0ppuW8_Rsn0NVMjbVXt8IJNNsaQ'
+BOT_USERNAME: Final = 'greenstein_bot'
